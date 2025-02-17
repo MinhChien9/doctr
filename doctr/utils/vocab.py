5fc2e372f9d499c14e9ce92d63372fff3991340e
Huỳnh Minh Chiến <chien17099@gmail.com>
2025-02-18 01:04:41 +0700

    fix: update abs path to relative path

--- doctr/utils/vocab.py
+++ doctr/utils/vocab.py
@@ -1,17 +1,18 @@
 import logging
 import platform
 import os
+from pathlib import Path
 
 __all__ = ["get_vocab"]
 
 
 def get_vocab(language: str):
-
-    vocab_path = os.path.join("doctr", "vocabs", f"{language}.txt")
+    data = ""
+    vocab_path = Path(__file__).parent.parent / "vocabs" / f"{language}.txt"
     if not os.path.exists(vocab_path):
         logging.warning(f"unable to load vocab at path : {vocab_path}")
-
-    with open(vocab_path, "r", encoding="utf-8") as f:
-        data = f.read()
+    else:
+        with open(vocab_path, "r", encoding="utf-8") as f:
+            data = f.read()
 
     return data
